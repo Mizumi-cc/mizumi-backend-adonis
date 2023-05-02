ef16edcae3e77613ec056357a343c831b1eecbfa
Timothy Yirenkyi <t.o.yirenkyi@gmail.com>
2023-05-02 09:33:23 +0000

    remove unused import

--- app/Controllers/Http/TransactionsController.ts
+++ app/Controllers/Http/TransactionsController.ts
@@ -7,7 +7,7 @@ import { IDL } from "App/Types/MizumiProgram";
 import { Connection, Keypair, PublicKey } from "@solana/web3.js";
 import { TRANSACTIONKIND, TRANSACTIONSTATUS, STABLES } from "App/Models/Enums";
 import DebitUserValidator from "App/Validators/DebitUserValidator";
-import { getAssociatedTokenAddressSync, getOrCreateAssociatedTokenAccount, TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import { getOrCreateAssociatedTokenAccount, TOKEN_PROGRAM_ID } from "@solana/spl-token";
 import CreditUserValidator from "App/Validators/CreditUserValidator";
 import CompleteTransactionValidator from "App/Validators/CompleteTransactionValidator";
 import Env from "@ioc:Adonis/Core/Env";
