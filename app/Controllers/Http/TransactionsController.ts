691823811d352708398b65e699e518da9a7384e7
Timothy Yirenkyi <t.o.yirenkyi@gmail.com>
2023-05-11 17:35:21 +0000

    fix

--- app/Controllers/Http/TransactionsController.ts
+++ app/Controllers/Http/TransactionsController.ts
@@ -324,11 +324,11 @@ export default class TransactionsController {
       })
     }
 
-    if (transaction.status !== TRANSACTIONSTATUS.DEBITED) {
-      return response.badRequest({
-        error: "Transaction not debited"
-      })
-    }
+    // if (transaction.status !== TRANSACTIONSTATUS.DEBITED) {
+    //   return response.badRequest({
+    //     error: "Transaction not debited"
+    //   })
+    // }
 
     transaction.status = TRANSACTIONSTATUS.SETTLING
     await transaction.save()
@@ -459,11 +459,11 @@ export default class TransactionsController {
       })
     }
 
-    if (transaction.status !== TRANSACTIONSTATUS.SETTLED) {
-      return response.badRequest({
-        error: "Transaction not settled"
-      })
-    }
+    // if (transaction.status !== TRANSACTIONSTATUS.SETTLED) {
+    //   return response.badRequest({
+    //     error: "Transaction not settled"
+    //   })
+    // }
 
     const userWallet = new PublicKey(user.walletAddress as string)
 
